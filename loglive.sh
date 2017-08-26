6877e4bdfec37ae5a7564dbc2142c8f3d91cbdeb
Ben Adamski <benjadamski@gmail.com>
2017-08-26 10:00:43 -0400

    Add --all flag to render all branches
    - Even those not diverged from relevant to current branch
    - Of course, don't know how I missed that, it's kind of the point.

--- loglive.sh
+++ loglive.sh
@@ -3,7 +3,7 @@
 while : 
 do
     clear
-   git --no-pager log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
+   git --no-pager log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
     sleep 1
 done
 
