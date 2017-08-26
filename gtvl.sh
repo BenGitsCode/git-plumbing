940ec9bb47031d7f3c2247567b097cba66d6f494
Ben Adamski <benjadamski@gmail.com>
2017-08-26 02:11:13 -0400

    It might be working ¯\_(ツ)_/¯
    - This commit will test whether the live log is
    working properly, then I can focus on making any
    adjustments to it.

--- gtvl.sh
+++ gtvl.sh
@@ -3,6 +3,10 @@
 while : 
 do
     clear
-    git --no-pager log -$1 --graph --all --pretty=format: '%(red%h%(reset -%((yellow)%d%(reset %s %(green(%cr) XC((boldblue)<%an>%(reset' --abbrev-commit --date-relative
+   git --no-pager log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'
     sleep 1
 done
+
+
+# original
+# git --no-pager log -$1 --graph --all --pretty=format: '%(red%h%(reset -%( (yellow)%d%(reset %s %(green(%cr) XC((boldblue)<%an>%(reset' --abbrev-commit --date-relative
